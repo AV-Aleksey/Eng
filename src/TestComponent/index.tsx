4373f416ebaf97b348dfe6feab86ad3e1f94ffc6
Aleksey-AV <aleksey.webmake@gmail.com>
2021-11-21 00:06:04 +0300

    main.base final

--- src/TestComponent/index.tsx
+++ src/TestComponent/index.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import css from './styles.module.css';
 
-const unused = "something";
+export const TestComponent = () => {
+    const [state, setState] = useState(0)
 
-export const TestComponent = () => <h2 className={css.Title}>12345</h2>
+    return (
+        <div>
+            <h2 className={css.Title}>{state}</h2>
+            <button onClick={() => setState((prev) => prev + 1)}>Add count</button>
+        </div>
+    )
+}
