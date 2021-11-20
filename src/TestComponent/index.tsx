7d961736014dcfd5762ffc6ebffa3f61d4be8ab4
Aleksey-AV <aleksey.webmake@gmail.com>
2021-11-20 23:37:01 +0300

    dev pack

---
+++ src/TestComponent/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import css from './styles.module.css';
+
+export const TestComponent = () => <h2 className={css.Title}>12345</h2>
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { TestComponent } from "./TestComponent";
+
+const App = () => (
+    <>
+        <h1>My React and TS App!</h1>
+        <TestComponent />
+    </>
+);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
